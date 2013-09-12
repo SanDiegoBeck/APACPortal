80b243c99e7c53607976890a4cab77d4d9d67128
陆秋石 <uicestone@gmail.com>
2013-09-12 13:03:16 +0800

    fill in test text in events

--- wp-content/themes/apacportal/page-index1.php
+++ wp-content/themes/apacportal/page-index1.php
@@ -14,6 +14,23 @@
 						<span class="more-link">More</span>
 					</header>
 					<div class="content">
+						<ul>
+							<li><a href="#">Mopar® at the 2013 Frankfurt International Motor Show</a></li>
+							<li><a href="#">Panda 4x4 Antartica, Freemont Black Code, new 500 engines at Frankfurt 2013</a></li>
+							<li><a href="#">FIAT Freestyle Team is official sponsor of Vans Downtown Showdown 2013</a></li>
+							<li><a href="#">Bielsko Biala receives the prestigious Automotive Lean Production Award </a></li>
+							<li><a href="#">Fiat is Technical Sponsor of the ‘World Masters Games 2013’.</a></li>
+							<li><a href="#">Mopar® at the 2013 Frankfurt International Motor Show</a></li>
+							<li><a href="#">Panda 4x4 Antartica, Freemont Black Code, new 500 engines at Frankfurt 2013</a></li>
+							<li><a href="#">FIAT Freestyle Team is official sponsor of Vans Downtown Showdown 2013</a></li>
+							<li><a href="#">Bielsko Biala receives the prestigious Automotive Lean Production Award </a></li>
+							<li><a href="#">Fiat is Technical Sponsor of the ‘World Masters Games 2013’.</a></li>
+							<li><a href="#">Mopar® at the 2013 Frankfurt International Motor Show</a></li>
+							<li><a href="#">Panda 4x4 Antartica, Freemont Black Code, new 500 engines at Frankfurt 2013</a></li>
+							<li><a href="#">FIAT Freestyle Team is official sponsor of Vans Downtown Showdown 2013</a></li>
+							<li><a href="#">Bielsko Biala receives the prestigious Automotive Lean Production Award </a></li>
+							<li><a href="#">Fiat is Technical Sponsor of the ‘World Masters Games 2013’.</a></li>
+						</ul>
 					</div>
 				</div>
 			</div>
