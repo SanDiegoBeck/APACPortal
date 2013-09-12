dd88f893a851156681fef1807d2c2d6f38966e43
陆秋石 <uicestone@gmail.com>
2013-09-12 12:40:33 +0800

    draft of index page (2 versions)

---
+++ wp-content/themes/apacportal/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+<!--	<link rel='stylesheet' href='<?=get_stylesheet_directory_uri()?>/bootstrap/css/bootstrap.min.css' type='text/css' media='all' />-->
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="page" class="hfeed site">
+		<header id="masthead" class="site-header" role="banner">
+			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				<h1 class="site-title"><img src="<?=get_stylesheet_directory_uri()?>/images/logo.png" alt="<?php bloginfo( 'name' ); ?>"></h1>
+				<h1 class="logo-aside"><img src="<?=get_stylesheet_directory_uri()?>/images/logo-apacportal.png"></h1>
+				<div class="description">
+					<span class="worldtime">
+						<span class="city">Turin: </span><span class="time">5:00am</span>
+						<span class="city">Shanghai: </span><span class="time">11:00am</span>
+					<span>
+					<span class="market">
+						SHARE PRICE  6.23€ + 1.71
+					<span>
+				</div>
+			</a>
+
+			<div id="navbar" class="navbar">
+				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
+					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
+					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+					<?php get_search_form(); ?>
+				</nav><!-- #site-navigation -->
+			</div><!-- #navbar -->
+		</header><!-- #masthead -->
+
+		<div id="main" class="site-main">
---
+++ wp-content/themes/apacportal/page-index2.php
@@ -0,0 +1,91 @@
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<img src="<?=get_stylesheet_directory_uri()?>/images/news_2.jpg" style="position:absolute;width:767px;height:200px;">
+			<div id="left" style="margin-top:210px;">
+				<div class="box">
+					<header>People Finder</header>
+					<div class="content">
+						<input type="text" placeholder="Search..." style="width: 120px"><button type="submit">Search</button>
+					</div>
+				</div>
+				<div class="box">
+					<header>Events
+						<span class="more-link">More</span>
+					</header>
+					<div class="content">
+					</div>
+				</div>
+			</div>
+			<div id="main" style="margin-top:210px;">
+				<div class="box">
+					<header>APAC News
+						<span class="more-link">More</span>
+					</header>
+					<div class="content">
+						<ul>
+							<li style="float: left; margin-right: 10px;"><a href="#"><img style="height:100px;" src="<?=get_stylesheet_directory_uri()?>/images/news_1.jpg"></a></li>
+							<li><a href="#">Mopar® at the 2013 Frankfurt International Motor Show</a></li>
+							<li><a href="#">Panda 4x4 Antartica, Freemont Black Code, new 500 engines at Frankfurt 2013</a></li>
+							<li><a href="#">FIAT Freestyle Team is official sponsor of Vans Downtown Showdown 2013</a></li>
+							<li><a href="#">Bielsko Biala receives the prestigious Automotive Lean Production Award </a></li>
+							<li><a href="#">Fiat is Technical Sponsor of the ‘World Masters Games 2013’.</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="box">
+					<header>Internal News
+						<span class="more-link">More</span>
+					</header>
+					<div class="content">
+						<ul>
+							<li><a href="#">Mopar® at the 2013 Frankfurt International Motor Show</a></li>
+							<li><a href="#">Panda 4x4 Antartica, Freemont Black Code, new 500 engines at Frankfurt 2013</a></li>
+							<li><a href="#">FIAT Freestyle Team is official sponsor of Vans Downtown Showdown 2013</a></li>
+							<li><a href="#">Bielsko Biala receives the prestigious Automotive Lean Production Award </a></li>
+							<li><a href="#">Fiat is Technical Sponsor of the ‘World Masters Games 2013’.</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="box">
+					<header>Global News
+						<span class="more-link">More</span>
+					</header>
+					<div class="content">
+						<ul>
+							<li><a href="#">Mopar® at the 2013 Frankfurt International Motor Show</a></li>
+							<li><a href="#">Panda 4x4 Antartica, Freemont Black Code, new 500 engines at Frankfurt 2013</a></li>
+							<li><a href="#">FIAT Freestyle Team is official sponsor of Vans Downtown Showdown 2013</a></li>
+							<li><a href="#">Bielsko Biala receives the prestigious Automotive Lean Production Award </a></li>
+							<li><a href="#">Fiat is Technical Sponsor of the ‘World Masters Games 2013’.</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div id="right">
+				<div class="box">
+					<header>Alerts/Notes
+						<span class="more-link">More</span>
+					</header>
+					<div class="content">
+						<ul>
+							<li><a href="#">Mopar® at the 2013 Frankfurt International Motor Show</a></li>
+							<li><a href="#">Panda 4x4 Antartica, Freemont Black Code, new 500 engines at Frankfurt 2013</a></li>
+							<li><a href="#">FIAT Freestyle Team is official sponsor of Vans Downtown Showdown 2013</a></li>
+							<li><a href="#">Bielsko Biala receives the prestigious Automotive Lean Production Award </a></li>
+							<li><a href="#">Fiat is Technical Sponsor of the ‘World Masters Games 2013’.</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="box">
+					<header>Weather</header>
+					<div class="content">
+					</div>
+				</div>
+			</div>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
