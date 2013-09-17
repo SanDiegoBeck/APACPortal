dd13b5c121d49d32293562f35734d344ae033a63
陆秋石 <uicestone@gmail.com>
2013-09-17 09:26:13 +0800

    Timezone order

--- wp-content/themes/apacportal/header.php
+++ wp-content/themes/apacportal/header.php
@@ -37,12 +37,13 @@
 				<h1 class="logo-aside"><img src="<?=get_stylesheet_directory_uri()?>/images/logo-apacportal.png"></h1>
 				<div class="description">
 					<span class="worldtime">
-						<?date_default_timezone_set('America/New_York');?>
-						<span class="city">Auburn Hills: </span><span class="time"><?=date('H:i')?></span>
-						<?date_default_timezone_set('Europe/Rome');?>
-						<span class="city">Turin: </span><span class="time"><?=date('H:i')?></span>
 						<?date_default_timezone_set('Asia/Shanghai');?>
 						<span class="city">Shanghai: </span><span class="time"><?=date('H:i')?></span>
+						<?date_default_timezone_set('Europe/Rome');?>
+						<span class="city">Turin: </span><span class="time"><?=date('H:i')?></span>
+						<?date_default_timezone_set('America/New_York');?>
+						<span class="city">Auburn Hills: </span><span class="time"><?=date('H:i')?></span>
+						<?date_default_timezone_set('Asia/Shanghai');?>
 					<span>
 				</div>
 			</a>
