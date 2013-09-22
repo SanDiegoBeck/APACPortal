3ce3cb6c52af73b17785942c8e5392a40b3fe2a6
陆秋石 <uicestone@gmail.com>
2013-09-22 08:44:56 +0800

    Work, Group & Core department pages

---
+++ wp-content/themes/apacportal/page-group.php
@@ -0,0 +1,42 @@
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content row" role="main">
+			<div class="col-md-3">
+				<?get_sidebar()?>
+			</div>
+			<div class="col-md-6">
+				<div class="box" style="height: 250px;">
+					<header>HR
+						<span class="more-link"><a href="/category/departments/ict/">More</a></span>
+					</header>
+					<div class="content">
+						<ul>
+							<li><a href="#">Mopar® at the 2013 Frankfurt International Motor Show</a></li>
+							<li><a href="#">Panda 4x4 Antartica, Freemont Black Code, new 500 engines at Frankfurt 2013</a></li>
+							<li><a href="#">FIAT Freestyle Team is official sponsor of Vans Downtown Showdown 2013</a></li>
+							<li><a href="#">Bielsko Biala receives the prestigious Automotive Lean Production Award </a></li>
+							<li><a href="#">Fiat is Technical Sponsor of the ‘World Masters Games 2013’.</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="box">
+					<header>Admin</header>
+					<div class="content">
+						<ul>
+							<li><a href="#">Mopar® at the 2013 Frankfurt International Motor Show</a></li>
+							<li><a href="#">Panda 4x4 Antartica, Freemont Black Code, new 500 engines at Frankfurt 2013</a></li>
+							<li><a href="#">FIAT Freestyle Team is official sponsor of Vans Downtown Showdown 2013</a></li>
+							<li><a href="#">Bielsko Biala receives the prestigious Automotive Lean Production Award </a></li>
+							<li><a href="#">Fiat is Technical Sponsor of the ‘World Masters Games 2013’.</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-right">
+				<?get_sidebar('department-list')?>
+			</div>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/apacportal/sidebar-department-list.php
@@ -0,0 +1,12 @@
+				<div class="box">
+					<header>Departments</header>
+					<div class="content">
+						<ul>
+							<li><a href="/admin" target="_blank">Admin</a></li>
+							<li><a href="/hr" target="_blank">HR</a></li>
+							<li><a href="/marketing" target="_blank">Marketing</a></li>
+							<li><a href="/communication" target="_blank">Communication</a></li>
+							<li><a href="/ict" target="_blank">ICT</a></li>
+						</ul>
+					</div>
+				</div>
