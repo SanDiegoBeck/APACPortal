66661fb23d045501b4d498732e6aaf59746e3377
陆秋石 <uicestone@gmail.com>
2013-09-22 10:14:05 +0800

    logo path fix; department list

--- wp-content/themes/apacportal/sidebar-department-list.php
+++ wp-content/themes/apacportal/sidebar-department-list.php
@@ -2,11 +2,10 @@
 					<header>Departments</header>
 					<div class="content">
 						<ul>
-							<li><a href="/admin" target="_blank">Admin</a></li>
-							<li><a href="/hr" target="_blank">HR</a></li>
-							<li><a href="/marketing" target="_blank">Marketing</a></li>
-							<li><a href="/communication" target="_blank">Communication</a></li>
-							<li><a href="/ict" target="_blank">ICT</a></li>
+							<li><a href="/Human Resource" target="_blank">HR</a></li>
+							<li><a href="/Marketing" target="_blank">Marketing</a></li>
+							<li><a href="/Communication" target="_blank">Communication</a></li>
+							<li><a href="/ICT" target="_blank">ICT</a></li>
 						</ul>
 					</div>
 				</div>
