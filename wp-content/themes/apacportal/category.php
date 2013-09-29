7d43af72d74bcc48efaabd7039d477ee0456b9ae
陆秋石 <uicestone@gmail.com>
2013-09-29 14:52:47 +0800

    category list style

--- wp-content/themes/apacportal/category.php
+++ wp-content/themes/apacportal/category.php
@@ -30,49 +30,24 @@ get_header(); ?>
 				<?php /* The loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<header class="entry-header">
-							<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-							<div class="entry-thumbnail">
-								<?php the_post_thumbnail(); ?>
-							</div>
-							<?php endif; ?>
-
-							<?php if ( is_single() ) : ?>
-							<h1 class="entry-title"><?php the_title(); ?></h1>
-							<?php else : ?>
-							<h1 class="entry-title">
-								<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-							</h1>
-							<?php endif; // is_single() ?>
-
-							<div class="entry-meta">
-								<?php twentythirteen_entry_meta(); ?>
-								<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
-							</div><!-- .entry-meta -->
-						</header><!-- .entry-header -->
-
-						<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-						<div class="entry-summary">
-							<?php the_excerpt(); ?>
-						</div><!-- .entry-summary -->
-						<?php else : ?>
-						<div class="entry-content">
-							<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-						</div><!-- .entry-content -->
-						<?php endif; ?>
-
-						<footer class="entry-meta">
-							<?php if ( comments_open() && ! is_single() ) : ?>
-								<div class="comments-link">
-									<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'twentythirteen' ) . '</span>', __( 'One comment so far', 'twentythirteen' ), __( 'View all % comments', 'twentythirteen' ) ); ?>
-								</div><!-- .comments-link -->
-							<?php endif; // comments_open() ?>
-
-							<?php if ( is_single() && get_the_author_meta( 'description' ) && is_multi_author() ) : ?>
-								<?php get_template_part( 'author-bio' ); ?>
-							<?php endif; ?>
-						</footer><!-- .entry-meta -->
+						<h1 class="entry-title">
+							<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+						</h1>
+						<dl class="dl-horizontal">
+							<dt>
+								<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+								<div class="entry-thumbnail">
+									<?php the_post_thumbnail(); ?>
+									&nbsp;
+								</div>
+								<? endif; ?>
+							</dt>
+							<dd>
+								<?php echo str_get_excerpt(get_the_content(),250); ?>
+							</dd>
+						</dl>
 					</article><!-- #post -->
+					<hr>
 				<?php endwhile; ?>
 				</div>
 				
