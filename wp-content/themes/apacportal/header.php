ed55931ad1ea3ee50f60f743e58c510d8d845bed
陆秋石 <uicestone@gmail.com>
2013-10-14 23:58:37 +0800

    trying to fix the intranet copatibility mode issue

--- wp-content/themes/apacportal/header.php
+++ wp-content/themes/apacportal/header.php
@@ -1,3 +1,4 @@
+<?header('X-UA-Compatible: IE=edge,chrome=1')?>
 <!DOCTYPE html>
 <!--[if IE 7]>
 <html class="ie ie7" <?php language_attributes(); ?>>
@@ -11,7 +12,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
