704b59b3ba70bbcb03f223e9b374731c486d99c9
陆秋石 <uicestone@gmail.com>
2013-10-22 15:45:09 +0800

    marketing sliders

--- wp-content/themes/apacportal/page-marketing.php
+++ wp-content/themes/apacportal/page-marketing.php
@@ -14,39 +14,60 @@
 				<header>Fiat APAC Marketing
 					<span class="more-link"><a href="/category/departments/marketing/fiat-apac-marketing">More</a></span>
 				</header>
-				<div class="content">
-					<ul>
-						<?query_posts('category_name=fiat-apac-marketing&posts_per_page=5')?>
-						<?while(have_posts()):the_post();?>
-						<li title="<?the_title()?>"><a href="<?the_permalink()?>" target="_blank"><?the_title()?></a></li>
-						<?endwhile;?>
-					</ul>
+				<div class="content" style="min-height: 0; height:188px; padding: 0;">
+					<div class="slider" id="slider192" style="width: 526px; height:188px; position: absolute;"> 
+						<div class="sliderContent" style="width: 526px; height:188px">
+							<?query_posts('category_name=fiat-apac-marketing&posts_per_page=3')?>
+							<?while(have_posts()):the_post();?>
+							<div class="item" style="width:526px; height:188px">
+								<a href="<?the_permalink()?>">
+									<?the_post_thumbnail('home-news-slider')?>
+								</a>
+								<summary><?the_title()?></summary>
+							</div>
+							<?endwhile;?>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="box">
 				<header>Alfa Romeo APAC Marketing
 					<span class="more-link"><a href="/category/departments/marketing/alfa-romeo-apac-marketing">More</a></span>
 				</header>
-				<div class="content">
-					<ul>
-						<?query_posts('category_name=alfa-romeo-apac-marketing&posts_per_page=5')?>
-						<?while(have_posts()):the_post();?>
-						<li title="<?the_title()?>"><a href="<?the_permalink()?>" target="_blank"><?the_title()?></a></li>
-						<?endwhile;?>
-					</ul>
+				<div class="content" style="min-height: 0; height:188px; padding: 0;">
+					<div class="slider" id="slider192" style="width: 526px; height:188px; position: absolute;"> 
+						<div class="sliderContent" style="width: 526px; height:188px">
+							<?query_posts('category_name=alfa-romeo-apac-marketing&posts_per_page=3')?>
+							<?while(have_posts()):the_post();?>
+							<div class="item" style="width:526px; height:188px">
+								<a href="<?the_permalink()?>">
+									<?the_post_thumbnail('home-news-slider')?>
+								</a>
+								<summary><?the_title()?></summary>
+							</div>
+							<?endwhile;?>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="box">
 				<header> Jeep APAC Marketing
 					<span class="more-link"><a href="/category/departments/marketing/jeep-apac-marketing">More</a></span>
 				</header>
-				<div class="content">
-					<ul>
-						<?query_posts('category_name=jeep-apac-marketing&posts_per_page=5')?>
-						<?while(have_posts()):the_post();?>
-						<li title="<?the_title()?>"><a href="<?the_permalink()?>" target="_blank"><?the_title()?></a></li>
-						<?endwhile;?>
-					</ul>
+				<div class="content" style="min-height: 0; height:188px; padding: 0;">
+					<div class="slider" id="slider192" style="width: 526px; height:188px; position: absolute;"> 
+						<div class="sliderContent" style="width: 526px; height:188px">
+							<?query_posts('category_name=jeep-apac-marketing&posts_per_page=3')?>
+							<?while(have_posts()):the_post();?>
+							<div class="item" style="width:526px; height:188px">
+								<a href="<?the_permalink()?>">
+									<?the_post_thumbnail('home-news-slider')?>
+								</a>
+								<summary><?the_title()?></summary>
+							</div>
+							<?endwhile;?>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="box">
