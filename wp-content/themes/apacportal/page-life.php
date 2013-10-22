097d92b8ea212b2ac0d43b07858e5347fc33b1d1
陆秋石 <uicestone@gmail.com>
2013-10-22 13:36:02 +0800

    notices & quick links on life page

--- wp-content/themes/apacportal/page-life.php
+++ wp-content/themes/apacportal/page-life.php
@@ -32,15 +32,31 @@
 					</div>
 				</div>
 				<div class="box">
-					<header>Notices
+					<header>
+						<span class="more-link"><a href="/category/notices">More</a></span>
+						Notices
 					</header>
 					<div class="content">
+						<ul>
+							<?query_posts('category_name=notices&posts_per_page=5')?>
+							<?while(have_posts()):the_post();?>
+							<li title="<?the_title()?>"><a href="<?the_permalink()?>" target="_blank"><?the_title()?></a></li>
+							<?endwhile;?>
+						</ul>
 					</div>
 				</div>
 				<div class="box">
-					<header>Quick Links
+					<header>
+						<span class="more-link"><a href="/category/quick-links">More</a></span>
+						Quick Links
 					</header>
 					<div class="content">
+						<ul>
+							<?query_posts('category_name=quick-links&posts_per_page=5')?>
+							<?while(have_posts()):the_post();?>
+							<li title="<?the_title()?>"><a href="<?the_permalink()?>" target="_blank"><?the_title()?></a></li>
+							<?endwhile;?>
+						</ul>
 					</div>
 				</div>
 			</div>
