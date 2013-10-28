0a1523538a9573bf1563cc63997ed91c01389b19
陆秋石 <uicestone@gmail.com>
2013-10-28 15:59:46 +0800

    sql

--- _doc/sql.sql
+++ _doc/sql.sql
@@ -1,29 +1,27 @@
-insert ignore into wp_users (user_email) select email from _people;
+insert into `wp_quotescollection` (quote, author) select quote, author from _quote;
  
-insert into wp_usermeta (user_id,meta_key,meta_value)
-select wp_users.id,'first_name',_people.first_name
-from wp_users inner join _people on _people.email = wp_users.user_email;
+delete from wp_usermeta where user_id in (select id from wp_users where user_login = '');
+delete from wp_users where user_login = '';
  
+insert ignore into wp_users (user_email) select `e-mail` from _people;
 insert into wp_usermeta (user_id,meta_key,meta_value)
-select wp_users.id,'last_name',_people.last_name
-from wp_users inner join _people on _people.email = wp_users.user_email;
- 
+select wp_users.id,'first_name',_people.`first name`
+from wp_users inner join _people on _people.`e-mail` = wp_users.user_email;
 insert into wp_usermeta (user_id,meta_key,meta_value)
-select wp_users.id,'telephone',_people.telephone
-from wp_users inner join _people on _people.email = wp_users.user_email;
- 
+select wp_users.id,'last_name',_people.`last name`
+from wp_users inner join _people on _people.`e-mail` = wp_users.user_email;
 insert into wp_usermeta (user_id,meta_key,meta_value)
-select wp_users.id,'cellphone',_people.cellphone
-from wp_users inner join _people on _people.email = wp_users.user_email;
- 
+select wp_users.id,'telephone',_people.`telephone number`
+from wp_users inner join _people on _people.`e-mail` = wp_users.user_email;
 insert into wp_usermeta (user_id,meta_key,meta_value)
-select wp_users.id,'department',_people.department
-from wp_users inner join _people on _people.email = wp_users.user_email;
- 
+select wp_users.id,'cellphone',_people.`cellphone number`
+from wp_users inner join _people on _people.`e-mail` = wp_users.user_email;
 insert into wp_usermeta (user_id,meta_key,meta_value)
-select wp_users.id,'company_name',_people.company_name
-from wp_users inner join _people on _people.email = wp_users.user_email;
- 
+select wp_users.id,'department',_people.`department name`
+from wp_users inner join _people on _people.`e-mail` = wp_users.user_email;
 insert into wp_usermeta (user_id,meta_key,meta_value)
-select wp_users.id,'working_site_country',_people.working_site_country
-from wp_users inner join _people on _people.email = wp_users.user_email;
+select wp_users.id,'company_name',_people.`company name`
+from wp_users inner join _people on _people.`e-mail` = wp_users.user_email;
+insert into wp_usermeta (user_id,meta_key,meta_value)
+select wp_users.id,'working_site_country',_people.`working site country`
+from wp_users inner join _people on _people.`e-mail` = wp_users.user_email;
