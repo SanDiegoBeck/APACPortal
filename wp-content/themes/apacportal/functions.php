ec4207e4f3e34746a3459fd9b5a490bf3ed04893
陆秋石 <uicestone@gmail.com>
2014-03-11 11:24:33 +0800

    try to fix stock price

--- wp-content/themes/apacportal/functions.php
+++ wp-content/themes/apacportal/functions.php
@@ -4,7 +4,7 @@ header('X-UA-Compatible: IE=edge,chrome=1');
 add_image_size( 'home-news-slider', 526, 188, true );
 
 function share_price(){
-	return $content = file_get_contents('https://www.google.com/finance?cid=673373');
+	return $content = file_get_contents('https://www.google.com/finance?q=BIT%3AF&sq=fiat%20spa&sp=4&ei=U4EeU9jxFsSUwQPSlgE');
 }
 
 function str_get_excerpt($str,$length=28){
