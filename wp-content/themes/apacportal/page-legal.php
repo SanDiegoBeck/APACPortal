aff933b64df078c16cdd8d35529c683a47966785
陆秋石 <uicestone@gmail.com>
2014-04-03 15:51:00 +0800

    Powertrain page

---
+++ wp-content/themes/apacportal/page-legal.php
@@ -0,0 +1,88 @@
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area page-india">
+		<div id="content" class="site-content row-fluid" role="main">
+			<div class="span3">
+				<div class="box">
+					<header>Welcome to APAC Powertrain</header>
+				</div>
+				<div class="box market-list">
+					<header>
+						Organization
+						<span class="more-link"><a href="/category/department/powertrain/">More</a></span>
+					</header>
+					<div class="content">
+						<?=apacportal_post_list('')?>
+					</div>
+				</div>
+				<div class="box">
+					<header>
+						<span class="more-link"><a href="/category/department/powertrain/">More</a></span>
+						Products
+					</header>
+					<div class="content">
+						<?=apacportal_post_list('')?>
+					</div>
+				</div>
+				<div class="box">
+					<header>
+						<span class="more-link"><a href="/category/department/powertrain/">More</a></span>
+						Work Place
+					</header>
+					<div class="content">
+						<?=apacportal_post_list('')?>
+					</div>
+				</div>
+			</div>
+			<div class="span6">
+				<div class="box">
+					<header>
+						<span class="more-link"><a href="/category/department/powertrain/">More</a></span>
+						News
+					</header>
+					<div class="content">
+						<?=apacportal_post_list('')?>
+					</div>
+				</div>
+				<div class="box">
+					<header>
+						<span class="more-link"><a href="/category/department/powertrain/">More</a></span>
+						Services
+					</header>
+					<div class="content">
+						<?=apacportal_post_list('')?>
+					</div>
+				</div>
+				<div class="box">
+					<header>
+						<span class="more-link"><a href="/category/department/powertrain/">More</a></span>
+						Travel
+					</header>
+					<div class="content">
+						<?=apacportal_post_list('')?>
+					</div>
+				</div>
+			</div>
+			<div class="span3 col-right">
+				<div class="box">
+					<header>
+						<span class="more-link"><a href="/category/department/powertrain/">More</a></span>
+						Pays & Benefits
+					</header>
+					<div class="content">
+						<?=apacportal_post_list('')?>
+					</div>
+				</div>
+				<div class="box">
+					<header>
+						<span class="more-link"><a href="/category/department/powertrain/">More</a></span>
+						Quick Links
+					</header>
+					<div class="content">
+						<?=apacportal_post_list('')?>
+					</div>
+				</div>
+			</div>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
