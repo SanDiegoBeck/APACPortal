a92f13cbcf6d5e2cbaa14fa58521d751c1e48d1c
Uice Lu <uicestone@gmail.com>
2014-04-15 17:26:32 +0800

    beautify search form

--- wp-content/themes/apacportal/searchform.php
+++ wp-content/themes/apacportal/searchform.php
@@ -1,4 +1,4 @@
-<form class="navbar-search search-form pull-right">
-	<input type="text" name="s" placeholder="Search for:" class="search-query search-field folded">
+<form class="navbar-search pull-right">
+	<input type="text" name="s" placeholder="Search for:" class="search-query folded">
 	<button type="submit" class="hidden">Search</button>
 </form>
