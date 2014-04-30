9f7e9fb90c5a6001453db067e4c35c110a3fd6a9
Uice Lu <uicestone@gmail.com>
2014-04-30 13:40:50 +0800

    improve archive page and search result page

--- wp-content/themes/apacportal/searchform.php
+++ wp-content/themes/apacportal/searchform.php
@@ -1,4 +1,4 @@
-<form class="navbar-search pull-right">
+<form action="<?=site_url()?>" class="navbar-search pull-right">
 	<input type="text" name="s" placeholder="Search for:" class="search-query folded">
 	<button type="submit" class="hidden">Search</button>
 </form>
