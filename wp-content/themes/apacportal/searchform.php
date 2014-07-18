1c8e45ea4291dbca290169eadfb6732dd3191ab1
Uice Lu <uicestone@gmail.com>
2014-07-18 11:23:20 +0800

    search through GSA

--- wp-content/themes/apacportal/searchform.php
+++ wp-content/themes/apacportal/searchform.php
@@ -1,4 +1,13 @@
-<form action="<?=site_url()?>" class="navbar-search pull-right">
-	<input type="text" name="s" placeholder="" class="search-query folded">
-	<button type="submit" class="hidden">Search</button>
+<form action="http://spsearch.fiat.com/search" class="navbar-search pull-right">
+    <input type="text" name="q" placeholder="" class="search-query folded">
+    <button type="submit" class="hidden">Search</button>
+    <input type="hidden" name="output" value="xml_no_dtd">
+    <input type="hidden" name="proxystylesheet" value="apaconn_fiat_chrysler">
+    <input type="hidden" name="site" value="apaconn_fiat_chrysler">
+    <input type="hidden" name="client" value="apaconn_fiat_chrysler">
+    <input type="hidden" name="btnG" value="Search APAConnect">
+    <input type="hidden" name="oe" value="UTF-8">
+    <input type="hidden" name="ie" value="UTF-8">
+    <input type="hidden" name="exclude_apps" value="1">
+    <input type="hidden" name="access" value="p">
 </form>
