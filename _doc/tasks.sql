a74d99e3b444eff6a18a213b38a10d5452998ee0
Uice Lu <uicestone@gmail.com>
2014-08-11 10:34:14 +0800

    SQL task: exporting mentor application form data

--- _doc/tasks.sql
+++ _doc/tasks.sql
@@ -11,3 +11,17 @@ GROUP BY region.id;
 SELECT country, SUM(count) FROM region_stats GROUP BY country;
 
 SELECT COUNT(*) FROM log WHERE `client` NOT LIKE 'gsa-%' AND MONTH(time) = 7;
+
+select
+MAX(IF(meta_key = 'name_en', meta_value, '')) `English Name`,
+MAX(IF(meta_key = 'name_cn', meta_value, '')) `Chinese Name`,
+MAX(IF(meta_key = 'id', meta_value, '')) `TID/CID`,
+MAX(IF(meta_key = 'email', meta_value, '')) `Email`,
+MAX(IF(meta_key = 'location', meta_value, '')) `Location`,
+MAX(IF(meta_key = 'organization', meta_value, '')) `Organization`,
+MAX(IF(meta_key = 'department', meta_value, '')) `Department`,
+MAX(IF(meta_key = 'direct_manager', meta_value, '')) `Direct Manager`,
+MAX(IF(meta_key = 'hire_date', meta_value, '')) `Hire Date`,
+MAX(IF(meta_key = 'reason', meta_value, '')) `Reason`,
+MAX(IF(meta_key = 'expectation', meta_value, '')) `Expectation`
+from wp_postmeta where post_id in (select ID from wp_posts where post_type = 'form_data') group by post_id;
