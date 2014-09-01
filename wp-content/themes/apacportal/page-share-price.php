def67ded32e7469967b0d756269e1f91003faaaa
Uice Lu <uicestone@gmail.com>
2014-09-01 23:02:08 +0800

    fix type in error message

--- wp-content/themes/apacportal/page-share-price.php
+++ wp-content/themes/apacportal/page-share-price.php
@@ -9,7 +9,7 @@ try{
 		$html = @file_get_contents('https://www.google.com/finance?q=BIT%3AF&sq=fiat%20spa&sp=4&ei=U4EeU9jxFsSUwQPSlgE');
 
 		if(!$html){
-			throw new Exception('Google Finance connection eefused');
+			throw new Exception('Google Finance connection refused');
 		}
 
 		$full_document = new DOMDocument();
