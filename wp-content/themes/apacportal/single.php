fb49dad329910e75bae7521f8fb414a4601a5845
Uice Lu <uicestone@gmail.com>
2014-09-30 11:33:01 +0800

    full width post content type

--- wp-content/themes/apacportal/single.php
+++ wp-content/themes/apacportal/single.php
@@ -12,10 +12,16 @@ get_header(); ?>
 <div id="primary" class="row-fluid content-area">
 	<div id="content" class="site-content clearfix" role="main">
 
+		<?php if(get_post_format() !== 'gallery'){ ?>
 		<div class="span3">
 			<?=do_shortcode('[sidebar id="left"]')?>
 		</div>
+		<?php } ?>
+		<?php if(get_post_format() !== 'gallery'){ ?>
 		<div class="span9">
+		<?php }else{ ?>
+		<div class="span12">
+		<?php } ?>
 		<?php /* The loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'content', get_post_format() ); ?>
