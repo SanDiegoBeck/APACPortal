9607609aface55caa98b6069c06c37aaa9808364
Uice Lu <uicestone@gmail.com>
2014-12-03 16:33:48 +0800

    fixing FIAT news by parsing HTML
    RSS from FIAT website was broken

--- wp-content/themes/apacportal/page-rss-feed.php
+++ wp-content/themes/apacportal/page-rss-feed.php
@@ -6,7 +6,7 @@ $feed_data = unserialize(get_option('feed_data'));
 
 if(empty($feed_data) || ($feed_data['timestamp'] < time() - 7200)){
 	$feed_data['chrysler'] = fetch_feed( 'http://scoop.chrysler.com/feed' );
-	$feed_data['fiat'] = fetch_feed( 'http://www.fcagroup.com/it-IT/Pages/RSS.aspx?Site=/en-us/media_center&FieldNameDesc=BoxTitle&FieldNameSort=ReferenceDate' );
+	$feed_data['fiat'] = file_get_html('http://www.fcagroup.com/en-us/_layouts/15/FIAT.Web.Publishing.VisualWebParts/FIATDocumentsPicker.aspx?isPriceSensitive=False&isEventViewer=False&subsite=en-us&showFilterByYear=True&listPath=en-US/media_center/fca_press_release/FiatDocuments&year=0')->find('.els-list ul li');
 	$feed_data['timestamp'] = time();
 	update_option('feed_data', serialize($feed_data));
 }
@@ -21,15 +21,6 @@ if ( ! is_wp_error( $feed_data['chrysler'] ) ){
 
 }
 
-if ( ! is_wp_error( $feed_data['fiat'] ) ){
-
-	// Figure out how many total items there are, but limit it to 5. 
-	$maxitems = $feed_data['fiat']->get_item_quantity( 3 ); 
-
-	// Build an array of all the items, starting with element 0 (first element).
-	$rss_items_fiat = $feed_data['fiat']->get_items( 0, $maxitems );
-
-}
 ?>
 <ul>
 	<?php if ( $maxitems == 0 ) : ?>
@@ -51,12 +42,13 @@ if ( ! is_wp_error( $feed_data['fiat'] ) ){
 		<li><?php _e( 'No items', 'my-text-domain' ); ?></li>
 	<?php else : ?>
 		<?php // Loop through each feed item and display each item as a hyperlink. ?>
-		<?php foreach ( (array)$rss_items_fiat as $item ) : ?>
+		<?php foreach ( (array)$feed_data['fiat'] as $index => $item ) : ?>
+			<?php if($index > 2) break; ?>
 			<li>
-				<a href="<?php echo esc_url( $item->get_permalink() ); ?>"
-					title="<?php printf( __( 'Posted %s', 'my-text-domain' ), $item->get_date('j F Y | g:i a') ); ?>"
+				<a href="http://www.fcagroup.com<?php echo esc_url( $item->find('a', 0)->href ); ?>"
+					title="<?php echo $item->find('p', 0); ?>"
 					target="_blank">
-					<?php echo str_replace('_',' ',esc_html( $item->get_title() )); ?>
+					<?php echo $item->find('p', 0)->find('text', 0); ?>
 				</a>
 			</li>
 		<?php endforeach; ?>
