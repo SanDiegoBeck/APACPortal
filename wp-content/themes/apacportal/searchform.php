66e92e8945d62346d1ae742d5571c91a47568ff8
Uice Lu <uicestone@gmail.com>
2014-12-24 12:09:42 +0800

    recover WordPress search form

--- wp-content/themes/apacportal/searchform.php
+++ wp-content/themes/apacportal/searchform.php
@@ -1,13 +1,4 @@
-<form action="http://spsearch.fiat.com/search" target="_blank" class="navbar-search pull-right">
-    <input type="text" name="q" placeholder="Search for..." class="search-query">
+<form action="<?=site_url()?>" target="_blank" class="navbar-search pull-right">
+    <input type="text" name="s" placeholder="Search for..." class="search-query">
     <button type="submit" class="hidden">Search</button>
-    <input type="hidden" name="output" value="xml_no_dtd">
-    <input type="hidden" name="proxystylesheet" value="apaconn_fiat_chrysler">
-    <input type="hidden" name="site" value="apaconn_fiat_chrysler">
-    <input type="hidden" name="client" value="apaconn_fiat_chrysler">
-    <input type="hidden" name="btnG" value="Search APAConnect">
-    <input type="hidden" name="oe" value="UTF-8">
-    <input type="hidden" name="ie" value="UTF-8">
-    <input type="hidden" name="exclude_apps" value="1">
-    <input type="hidden" name="access" value="p">
 </form>
